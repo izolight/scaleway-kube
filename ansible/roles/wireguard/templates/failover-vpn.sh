87617eac192a7cffaab6159b6d54f858e22604fe
Gabor Tanz <izolight@gmail.com>
2018-10-20 20:05:02 +0200

    add output and fix broken fallback

--- ansible/roles/wireguard/templates/failover-vpn.sh
+++ ansible/roles/wireguard/templates/failover-vpn.sh
@@ -7,20 +7,25 @@ RESULT=$?
 
 if [ $RESULT -eq 0 ];
 then
+	echo "Everything seems fine"
 	exit 0
 fi
 
+echo "Ping didn't return, endpoint is probably down"
 # get active service
 CURRENT=$(cat /tmp/vpn)
 DEFAULT=tunnel_0
 FALLBACK=tunnel_1
 
+echo "Stopping active vpn"
 systemctl stop wg-quick@$CURRENT
 
 if [ $CURRENT == $DEFAULT ];
 then
+	echo "Failing over to $FALLBACK"
 	systemctl start wg-quick@$FALLBACK
 elif [ $CURRENT == $FALLBACK ];
 then
-	systemctl start wg-quick@$CURRENT
+	echo "Failing over to $DEFAULT"
+	systemctl start wg-quick@$DEFAULT
 fi
